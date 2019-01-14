b7d55f2941614c6bfcfbfd24ce877fb47d4ba4ae
dam2a03 <dam2a03@SINF2003SRVR2.LOCAL>
2019-01-14 16:59:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dam2a03/splash/QuizActivity.java
@@ -0,0 +1,11 @@
+package com.example.dam2a03.splash;
+import android.app.Activity;
+
+public class QuizActivity extends Activity {
+
+    public static final String GAME_PREFERENCES = "GamePrefs";
+
+
+
+
+}
---
+++ app/src/main/java/com/example/dam2a03/splash/QuizGameActivity.java
@@ -0,0 +1,4 @@
+package com.example.dam2a03.splash;
+
+public class QuizGameActivity extends QuizActivity {
+}
